15da0063ffa65c008297d76cdd9bf131c8447a1d
Adel <adelmejrissi@gmail.com>
2025-12-03 21:56:04 +0100

    feat: added rest controllers an entity and a jpa repository

--- training/src/main/java/com/trainingArc/training/BookRestController.java
+++ training/src/main/java/com/trainingArc/training/BookRestController.java
@@ -1,17 +1,44 @@
 package com.trainingArc.training;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 public class BookRestController {
+
+    private final BookRepository bookRepository;
+
+    BookRestController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
 	
 	@GetMapping("/book")
-	public String get() {
+	public String get(@RequestParam String bookName, @RequestParam Integer bookPages) {
 		
+		System.out.println(bookName);
+		System.out.println(bookPages);
 		
-		
-		return "abcd";
+		BookEntity existingbook = bookRepository.findByNameAndPages(bookName, bookPages);
+
+		if (existingbook == null) {
+			
+			BookEntity newBook = BookEntity.builder()
+										.name(bookName)
+										.pages(bookPages)
+										.build();
+			
+			bookRepository.save(newBook);
+			
+			return "le livre a été crée";
+			
+		} else {
+			
+			return "le livre existe déjà";
+		}
 	}
 	
 }
