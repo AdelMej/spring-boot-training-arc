3d169dfa9ddf3a966935faadb3d7580841818fc0
Adel <adelmejrissi@gmail.com>
2025-12-12 09:04:45 +0100

    feat: added some more dto

--- training/src/main/java/com/trainingArc/training/book/dto/BookDTO.java
+++ training/src/main/java/com/trainingArc/training/book/dto/BookDTO.java
@@ -1,10 +1,18 @@
 package com.trainingArc.training.book.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
 public class BookDTO {
 
+	@Data
+	@AllArgsConstructor
+	@Builder
 	public static class PostInput {
 		
-		
+	String bookName;
+	Integer bookPages;
 		
 	}
 	
--- training/src/main/java/com/trainingArc/training/book/service/BookService.java
+++ training/src/main/java/com/trainingArc/training/book/service/BookService.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 import com.trainingArc.training.book.model.BookEntity;
 import com.trainingArc.training.book.persistence.BookRepository;
 
+import io.micrometer.common.util.StringUtils;
+
 @Service
 public class BookService {
 
@@ -14,8 +16,20 @@ public class BookService {
         this.bookRepository = bookRepository;
     }
 	
-	public String createBook(String bookName, Integer bookPages) {
+	public String createBook(String bookName, Integer bookPages) throws Exception {
+
+		if(bookName == null || StringUtils.isBlank(bookName)) {
+			
+			// return "Le bookname ne peut pas être vide ou null";
+			throw new Exception();
+		}
 		
+		if(bookPages == null || bookPages <= 0) {
+			
+			return "Le nombre de page ne peut pas être <= 0";
+			
+		}
+
 		BookEntity existingbook = bookRepository.findByNameAndPages(bookName, bookPages);
 
 		if (existingbook == null) {
